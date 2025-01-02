3c26b6efa20baeb059c6322383b945e0230d3392
sujonosyntax <imamsujono.syntax@gmail.com>
2025-01-02 16:13:26 +0700

    Menambah router

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,13 +9,37 @@ $routes->get('/', 'Home::index');
 
 use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+use App\Controllers\Humas;
+use App\Controllers\Surat;
+use App\Controllers\Sbml;
+use App\Controllers\Tracking;
 
 $routes->get('news', [News::class, 'index']);
 $routes->get('news/new', [News::class, 'new']); // Add this line
-$routes->get('news', [News::class, 'index']);           // Add this line
 $routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 
 $routes->post('news', 'News::create');
+
+// Features
+$routes->get('humas', [Humas::class, 'index']);
+// $routes->get('humas', [Humas::class, 'index']);
+// $routes->get('humas', [Humas::class, 'index']);
+// $routes->get('humas', [Humas::class, 'index']);
+
+$routes->get('surat', [Surat::class, 'index']);
+// $routes->get('surat', [Surat::class, 'index']);
+// $routes->get('surat', [Surat::class, 'index']);
+// $routes->get('surat', [Surat::class, 'index']);
+
+$routes->get('sbml', [Sbml::class, 'index']);
+// $routes->get('sbml', [Sbml::class, 'index']);
+// $routes->get('sbml', [Sbml::class, 'index']);
+// $routes->get('sbml', [Sbml::class, 'index']);
+
+$routes->get('tracking', [Tracking::class, 'index']);
+// $routes->get('tracking', [Tracking::class, 'index']);
+// $routes->get('tracking', [Tracking::class, 'index']);
+// $routes->get('tracking', [Tracking::class, 'index']);
