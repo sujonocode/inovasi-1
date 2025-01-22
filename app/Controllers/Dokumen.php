684ed8fd4a2eadf6b7385693d1695bdd02a8d1de
sujonosyntax <imamsujono.syntax@gmail.com>
2025-01-22 17:10:55 +0700

    Menambahkan dashboard

--- app/Controllers/Dokumen.php
+++ app/Controllers/Dokumen.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
 use App\Models\SuratModel;
 use App\Models\SKModel;
 use App\Models\KontrakModel;
@@ -10,15 +11,19 @@ class Dokumen extends BaseController
 {
     public function index(string $page = 'Manajemen Dokumen')
     {
+        $modelUser = new UserModel();
         $modelSurat = new SuratModel();
         $modelSK = new SKModel();
         $modelKontrak = new KontrakModel();
 
         $data = [
             'title' => ucfirst($page),
+
+            'totalUser' => $modelUser->countAll(),
             'totalSurat' => $modelSurat->countAll(),
             'totalSK' => $modelSK->countAll(),
             'totalKontrak' => $modelKontrak->countAll(),
+
             'totalSuratByKodeArsip' => $modelSurat
                 ->select('kode_arsip, COUNT(*) as count')
                 ->groupBy('kode_arsip')
@@ -30,6 +35,19 @@ class Dokumen extends BaseController
             'totalKontrakByKodeArsip' => $modelKontrak
                 ->select('kode_arsip, COUNT(*) as count')
                 ->groupBy('kode_arsip')
+                ->findAll(),
+
+            'totalSuratByCreatedBy' => $modelSurat
+                ->select('created_by, COUNT(*) as count')
+                ->groupBy('created_by')
+                ->findAll(),
+            'totalSKByCreatedBy' => $modelSK
+                ->select('created_by, COUNT(*) as count')
+                ->groupBy('created_by')
+                ->findAll(),
+            'totalKontrakByCreatedBy' => $modelKontrak
+                ->select('created_by, COUNT(*) as count')
+                ->groupBy('created_by')
                 ->findAll(),
         ];
 
